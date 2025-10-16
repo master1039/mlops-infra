e5147bca91cd95ab85e53f09f0701e6943c1c361
master1039 <saurabh.sinha212@gmail.com>
2025-10-16 11:29:02 +0530

    workflow final

--- infra/infra.sh
+++ infra/infra.sh
@@ -1,6 +1,6 @@
 RESOURCE_GROUP="MY_MLOPS_RG"
 LOCATION="eastus2"
-WORKSPACE_NAME="mlops-workspace-automation-new"
+WORKSPACE_NAME="mlops-workspace"
 CLUSTER_NAME="mlops-cluster"
 
 az group create -n $RESOURCE_GROUP -l $LOCATION
@@ -9,7 +9,7 @@ az ml workspace create -n $WORKSPACE_NAME -g $RESOURCE_GROUP -l $LOCATION
 
 az ml workspace set -g $RESOURCE_GROUP -w $WORKSPACE_NAME
 
-az ml create compute --name $CLUSTER_NAME \
+az ml compute create --name $CLUSTER_NAME \
                         --type amlcompute \
                         --size Standard_DS11_V2 \
                         --min-instances 0 \
